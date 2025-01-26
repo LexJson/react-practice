72e9bb1c209b0a56d37872f31a413c8c46444d92
Lexi Johnson <lej976@usask.ca>
2025-01-25 21:00:29 -0600

    Board displays Squares with data dependent on props sent from the Game component. Grid of Squares is fixed and with proper rows and columns.

--- minesweeper/src/App.tsx
+++ minesweeper/src/App.tsx
@@ -1,60 +1,80 @@
-// import "./styles.css";
 import { useState } from "react";
+// import { ReactPropTypes } from "react";
 
 /**
  * Component that displays a single square button.
  *
  * TODO: On click...
  */
-// Instead of function: const MyComponent = () => { <> ... </> }
-function Square({ row, column }) {
-  /** Square is initially not dug.
-   * Once dug is true, square displayes either a mine or adjacent mine number. */
-  // const [dug, setDug] = useState(false);
+function Square({ value }) {
+  //TODO: Different render depending on if square is dug or not.
 
   // TODO: onClick={onSquareClick}
-  console.log(row, column);
-  return <button className="square">{row + ", " + column}</button>;
+  return <button className="square">{value}</button>;
 }
 
 /**
  * Component that displays a grid of Square components.
- * Amount of rows and columns in the grid is passed from Game.
+ *
  * TODO: Handles...
  */
-function Board({ rows, columns }) {
+function Board({ minefield, history, rows, columns }) {
   function handleClick() {
     //TODO
   }
 
-  const boardCoordinateList = [];
-  // Fill index array
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < columns; j++) {
-      // Add square (i,j) to list
-      boardCoordinateList.push([i, j]);
-    }
-  }
-  // Map the indexes to squares for the board
-  // return ( {getMessages.map(message => ( <Message thisMessage={message}> </Message> ))} );
-  // <li key={coord}> cannot assign array as key // key={boardCoordinateList.indexOf(coord)}
-  const squareList = boardCoordinateList.map((coord) => {
-    // console.log(rowCoord, columnCoord);
-    return <Square row={coord[0]} column={coord[1]} />;
+  // Render the board from the given minefield array
+  const squareList = minefield.map((row, rowIndex) => {
+    const rowList = row.map((square, columnIndex) => {
+      // Render individual squares in a row
+      //TODO: Different render depending on if square is dug or not.
+      return <Square key={columnIndex} value={columnIndex} />;
+    });
+    // Render rows of squares
+    return (
+      <div className="board-row" key={rowIndex}>
+        {rowList}
+      </div>
+    );
   });
 
-  return <div className="board-row"> {squareList} </div>;
+  return <>{squareList}</>;
 }
 
 export default function Game() {
   // TODO: rows and columns comes from difficulty selected.
   const boardRows = 15;
   const boardColumns = 20;
+  /** Array(size of rows) of arrays(each the size of columns)
+   *  -1: mine
+   * >=0: number of adjacent mines
+   */
+  const [minefield, setMinefield] = useState(
+    Array(boardRows).fill(Array(boardColumns).fill(0))
+  );
+  //TODO: generate Minefield
+  // ...
+
+  /** Array(size of rows) of arrays(each the size of columns)
+   * Records where the player has...
+   * 0: right clicked to 'dig'
+   * 1: left clicked to 'place a flag'
+   */
+  const [history, setHistory] = useState(
+    Array(boardRows).fill(Array(boardColumns).fill(null))
+  );
 
   return (
     <div className="App">
-      <h2>This will be Minesweeper</h2>
-      <Board rows={boardRows} columns={boardColumns} />
+      <h2>This will be minesweeper</h2>
+      <div className="game">
+        <Board
+          minefield={minefield}
+          history={history}
+          rows={boardRows}
+          columns={boardColumns}
+        />
+      </div>
     </div>
   );
 }
