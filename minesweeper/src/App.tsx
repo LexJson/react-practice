9fa48701adb18b9ae0f8e47de031c120543d2ff3
Lexi Johnson <122935072+LexJson@users.noreply.github.com>
2025-03-02 14:40:18 -0600

    App fix post-separation
    Merge conflicts adjustment.

--- minesweeper/src/App.tsx
+++ minesweeper/src/App.tsx
@@ -1,65 +1,137 @@
-import React from "react";
 import { useState } from "react";
 // import { ReactPropTypes } from "react";
-
-/**
- * Component that displays a single square button.
- *
- * TODO: On click...
- */
-function Square({ value }) {
-  //TODO: Different render depending on if square is dug or not.
-
-  // TODO: onClick={onSquareClick}
-  return <button className="square">{value}</button>;
-}
-
-/**
- * Component that displays a grid of Square components.
- *
- * TODO: Handles...
- */
-function Board({ minefield, history, rows, columns }) {
-  function handleClick() {
-    //TODO
-  }
-
-  // Render the board from the given minefield array
-  const squareList = minefield.map((row, rowIndex) => {
-    const rowList = row.map((square, columnIndex) => {
-      // Render individual squares in a row
-      //TODO: Different render depending on if square is dug or not.
-      return <Square key={columnIndex} value={0} />;
-    });
-    // Render rows of squares
-    return (
-      <div className="board-row" key={rowIndex}>
-        {rowList}
-      </div>
-    );
-  });
-
-  return <>{squareList}</>;
-}
+import { Board } from "./Board";
+import calculateWin from "./CalculateWin";
 
 export default function Game() {
-  // TODO: rows and columns comes from difficulty selected.
-  const boardRows = 15;
-  const boardColumns = 20;
-  /** Array(size of rows) of arrays(each the size of columns)
-   *  -1: mine
-   * >=0: number of adjacent mines
-   */
-  const [minefield, setMinefield] = useState(
-    Array(boardRows).fill(Array(boardColumns).fill(0))
-  );
-  //TODO: generate Minefield
-  // ...
+  // TODO: values that come from difficulty selected
+  const boardRows = 15; // small:10, medium:15
+  const boardColumns = 20; // small:14, medium:20
+  const numberOfMines = 55; // small:25, medium:55
+  const [status, setStatus] = useState("");
+  const [flagsRemaining, setFlagsRemaining] = useState(0);
 
-  /** Array(size of rows) of arrays(each the size of columns)
-   * Records where the player has...
-   * 0: right clicked to 'dig'
-   * 1: left clicked to 'place a flag'
+  // Generate the minefield --------------------------------------------
+  // First populate a list with empty mine locations
+  // BAD - All arrays are the same: let initialMinefield = Array(boardRows).fill(Array(boardColumns).fill(0));
+  let initialMinefield = [];
+  for (let i = 0; i < boardRows; i++) {
+    // Each time create new row array
+    let newRow = [];
+    for (let j = 0; j < boardColumns; j++) {
+      // Add items to the current new row
+      newRow.push(0);
+    }
+    initialMinefield.push(newRow);
+  }
+
+  // List of all the available indexes for placing mines.
+  let availableIndexes = [];
+  for (let i = 0; i < boardRows; i++) {
+    for (let j = 0; j < boardColumns; j++) {
+      availableIndexes.push([i, j]);
+    }
+  }
+
+  // Place mines at random at available indexes.
+  for (let x = 0; x < numberOfMines; x++) {
+    // Get random pair of indexes.
+    // Math.floor(Math.random() * x) gives a number with no decimals ("integer") between 0 to x-1.
+    let randomIndex = Math.floor(Math.random() * availableIndexes.length);
+    let availableIndexesPair = availableIndexes[randomIndex];
+    let rowIndex = availableIndexesPair[0];
+    let columnIndex = availableIndexesPair[1];
+
+    // Set the mine at the random index.
+    initialMinefield[rowIndex][columnIndex] = -1;
+
+    // For debug:
+    // console.log(rowIndex, columnIndex);
+    // initialMinefield.forEach((item, i) => {
+    //   console.log("row ", i, ": ", item);
+    // });
+
+    // Remove the just used index from the available index list.
+    availableIndexes.splice(randomIndex, 1);
+  }
+
+  /**
+   * Returns the number of mines surrounding a given square,
+   * or -1 if the given square is a mine.
+   * Square is determined by the passed row and column indexes.
+   */
+  function countAdjacentMines(rowIndex, columnIndex) {
+    let adjacentMines = 0;
+
+    if (initialMinefield[rowIndex][columnIndex] != -1) {
+      // Check each of the 8 potential squares surrounding the given square.
+      const surroundingIndexes = [
+        [rowIndex - 1, columnIndex - 1], // top left
+        [rowIndex - 1, columnIndex], // top center
+        [rowIndex - 1, columnIndex + 1], // top right
+        [rowIndex, columnIndex - 1], // middle left
+        [rowIndex, columnIndex + 1], // middle right
+        [rowIndex + 1, columnIndex - 1], // bottom left
+        [rowIndex + 1, columnIndex], // bottom center
+        [rowIndex + 1, columnIndex + 1], // bottom right
+      ];
+
+      surroundingIndexes.forEach((indexes) => {
+        // Check the row and column indexes are within the range of the arrays.
+        if (
+          indexes[0] >= 0 &&
+          indexes[0] < boardRows &&
+          indexes[1] >= 0 &&
+          indexes[1] < boardColumns
+        ) {
+          // Index is valid, so check for a mine.
+          if (initialMinefield[indexes[0]][indexes[1]] == -1) {
+            adjacentMines++;
+          }
+        }
+      });
+    } else {
+      // Return the mine value (-1) to continue showing that square is a mine.
+      adjacentMines = -1;
+    }
+
+    return adjacentMines;
+  }
+
+  // Calculate adjacent mines for each square
+  for (let i = 0; i < boardRows; i++) {
+    // Each time create new row array
+    for (let j = 0; j < boardColumns; j++) {
+      // Add items to the current new row
+      let adjacentMines = countAdjacentMines(i, j);
+      initialMinefield[i][j] = adjacentMines;
+    }
+  }
+
+  /** Lists how many mines surround each mine.
+   *    -1: mine
+   *   0-8: number of adjacent mines
+   */
+  const [minefield, setMinefield] = useState(initialMinefield);
+  //-----------------------------------------------------------------
+
+  // Initialize array for flags and digs history
+  let initialHistory = [];
+  for (let i = 0; i < boardRows; i++) {
+    // Each time create new row array
+    let newRow = [];
+    for (let j = 0; j < boardColumns; j++) {
+      // Add items to the current new row
+      newRow.push(0);
+    }
+    initialHistory.push(newRow);
+  }
+  /**
+   * Records if each square has nothing, a dug spot, or a flag.
+   *     0: nothing, not dug and currently no flag
+   *     1: has been left clicked to 'dig'
+   *     2: has been right clicked to place a 'flag'
+   *        (Note: Flags can be placed and removed with right click)
    */
   const [flagsAndDigs, setFlagsAndDigs] = useState(initialHistory);
 
@@ -109,48 +181,4 @@ export default function Game() {
       </div>
     </div>
   );
-}
-
-/**
- * Determines if the game has been won, lost, or is still going.
- * returns: true if the game is won, false if the game is lost, null if the game is not done yet.
- */
-function calculateWin(
-  minefield,
-  flagsAndDigs,
-  boardRows,
-  boardColumns,
-  numberOfMines
-) {
-  // Loss: If any dug squares have a mine, game is lost.
-  // Win: If all possible squares have been dug, meaning only mines remain un-dug.
-
-  // Number of dug squares, according to the flagsAndDigs list.
-  let numberOfDigs = 0;
-
-  // Find each dug square and determine if it was a mine
-  for (let i = 0; i < boardRows; i++) {
-    for (let j = 0; j < boardColumns; j++) {
-      // Check each item and its dig state
-      if (flagsAndDigs[i][j] == 1) {
-        numberOfDigs++;
-        if (minefield[i][j] == -1) {
-          // Game was lost, return false to notify the Board
-          console.log("Game Lost");
-          return false;
-        }
-      }
-    }
-  }
-  // No mines have been dug.
-  // Check if all possible squares have been dug.
-  if (numberOfDigs == boardRows * boardColumns - numberOfMines) {
-    // Game was won, return true to notify the Board.
-    console.log("Game Won");
-    return true;
-  } else {
-    // Game is still going, return null to signify it's not won or lost.
-    console.log("Game Ongoing");
-    return null;
-  }
 }
